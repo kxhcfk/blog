a0e1c41930feb93f2db24cfe83b55fbe8fa3327e
kxhcfk <reveersed@mail.ru>
2023-09-14 16:52:48 +0300

    feat: stylelint, i18next esling plugin

--- src/features/LangSwitcher/ui/index.tsx
+++ src/features/LangSwitcher/ui/index.tsx
@@ -1,34 +1,32 @@
-import React, {ButtonHTMLAttributes} from "react";
+import React, { ButtonHTMLAttributes } from 'react';
 
-import {classNames} from "@/shared/lib";
+import { useTranslation } from 'react-i18next';
 
-import styles from "./styles.module.scss";
-import {useTranslation} from "react-i18next";
-import {Button, ThemeButton} from "@/shared/ui/Button";
+import { Button, ThemeButton } from '@/shared/ui/Button';
 
 interface LangSwitcherProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const LangSwitcher = (props: LangSwitcherProps) => {
-    const {className, onClick, ...otherProps} = props;
-    
-    const {t, i18n} = useTranslation();
-    
+    const { className, onClick, ...otherProps } = props;
+
+    const { t, i18n } = useTranslation();
+
     const handleLangToggle = (e: React.MouseEvent<HTMLButtonElement>) => {
-        i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
         onClick?.(e);
     };
-    
+
     return (
         <Button
             theme={ThemeButton.CLEAR}
             onClick={handleLangToggle}
-            className={classNames(styles.root, className)}
+            className={className}
             {...otherProps}
         >
-            {t("Язык")}
+            {t('Язык')}
         </Button>
     );
 };
 
-export {LangSwitcher};
+export { LangSwitcher };
